d66dfbf7264b5ee12f034dfcbb1767cfbd60aba0
jhonyyydev <jvelozamalaver@gmail.com>
2025-07-26 12:19:14 -0500

    feat: definición de rutas en routes/api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\InstitutionController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Rutas públicas
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    // User informacion
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Institutions
+    Route::get('/institutions', [InstitutionController::class, 'index']);
+    Route::post('/institutions', [InstitutionController::class, 'store']);
+});
